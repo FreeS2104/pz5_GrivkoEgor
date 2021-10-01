2f8558f9e33250e877364e87a1e5cd8b38907c42
Егор Гривко <kombad56@mail.ru>
2021-10-01 13:14:14 +0500

    test chek

---
+++ pz6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            int n = int.Parse(Console.ReadLine());
+            while (n % i != 0) 
+            i += 1;
+            Console.WriteLine(i);
+            ////
+           
+
+
+        }
+    }
+}
